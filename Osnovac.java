baaf8f2256125802698b36ea7ae4a607fab9c027
Gordan Sajević <gsajevic@hotmail.com>
2014-12-17 14:10:34 +0100

    Create Osnovac.java

---
+++ Osnovac.java
@@ -0,0 +1,17 @@
+package lab26;
+
+public class Osnovac extends Prolaznik{
+
+	//Zadatak sa predavanja
+	
+	/**
+	 * Konstruktor bez parametara
+	 */
+	
+	public String getOdgovor()
+	{
+		return "Aha";
+	}
+	
+	
+}
