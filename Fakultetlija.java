979efaa83c46deea4899157470d1ff9e89105305
Gordan Sajević <gsajevic@hotmail.com>
2014-12-17 14:11:13 +0100

    Create Fakultetlija.java

---
+++ Fakultetlija.java
@@ -0,0 +1,17 @@
+package lab26;
+
+public class Fakultetlija extends Prolaznik{
+	
+	//Zadatak sa predavanja
+	
+		/**
+		 * Konstruktor bez parametara
+		 */
+		
+	
+	public String getOdgovor()
+	{
+		return "Jeste";
+	}
+
+}
