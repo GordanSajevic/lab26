714515c5e5d82b0a187a5e5f45139495935b19bf
Gordan Sajević <gsajevic@hotmail.com>
2014-12-17 14:10:53 +0100

    Create Srenjoskolac.java

---
+++ Srenjoskolac.java
@@ -0,0 +1,15 @@
+package lab26;
+
+public class Srednjoskolac extends Prolaznik{
+
+	//Zadatak sa predavanja
+	
+	/**
+	 * Konstruktor bez parametara
+	 */
+	
+	public String getOdgovor()
+	{
+		return "Da";
+	}
+}
