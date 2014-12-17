43f23ad014ea17054fd41cdc5785ce32d5408e16
Gordan Sajević <gsajevic@hotmail.com>
2014-12-17 14:10:02 +0100

    Create Prolaznik.java

---
+++ Prolaznik.java
@@ -0,0 +1,28 @@
+package lab26;
+
+public class Prolaznik {
+	
+	//Zadatak sa predavanja
+	
+	private String odgovor;
+	
+	/**
+	 * Konstruktor bez parametara
+	 */
+	
+	public Prolaznik()
+	{
+		this.odgovor = "Mrš!";
+	}
+	
+	/**
+	 * Getter za odgovor
+	 * @return
+	 */
+	
+	public String getOdgovor()
+	{
+		return odgovor;
+	}
+
+}
